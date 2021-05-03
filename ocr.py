fc91583ad86a7b21813a179725c385ac2361bece
BabyYoda741 <nikhilanand741@gmail.com>
2021-05-03 14:26:12 +0530

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -1,5 +1,5 @@
 from tkinter import *
-#from tkinter import messagebox
+from tkinter import messagebox
 #from tkinter import ttk
 import cv2
 from cv2 import cv2
@@ -14,61 +14,95 @@ class Ocr:
     def __init__(self, root):
         self.root = root
         self.root.title("Optical Character Recognition")
-        self.root.geometry("1280x720+120+10")
+        self.root.geometry("900x650+320+100")
+        self.root.configure(bg='cyan3')
+        self.img=''
+        fname2=''
 
-        F1 = Frame(self.root)
-        F1.place(x=0, y=30, width=640, height=50)
-        Button(F1,width=12,text="Select Image",font=("arial", 13), bg="gray80", bd=1, command=self.selectimg).grid(padx=10,pady=3,row=0, column=0)
-        Button(F1,width=12,text="Run OCR",font=("arial", 13), bg="gray80", bd=1, command=self.scan).grid(padx=10,pady=3,row=0, column=1)
-
+        F1 = Frame(self.root,bg='cyan3')
+        F1.place(x=130, y=30, width=900, height=50)
+        Button(F1,width=12,text="Open Image",font=("arial", 13), bg="orange", bd=1, command=self.selectimg).grid(padx=15,pady=3,row=0, column=0)
+        Button(F1,width=12,text="Run OCR",font=("arial", 13), bg="orange", bd=1, command=self.scan).grid(padx=15,pady=3,row=0, column=1)
+        Button(F1,width=12,text="Save as Text",font=("arial", 13), bg="orange", bd=1, command=self.save).grid(padx=15,pady=3,row=0, column=2)
+        Button(F1,width=15,text="Open in Notepad",font=("arial", 13), bg="orange", bd=1, command=self.opennp).grid(padx=15,pady=3,row=0, column=3)
+        
         F2 = Frame(self.root)
-        F2.place(x=640, y=80, width=640, height=620)
+        F2.place(x=40, y=80, width=820, height=500)
         scroll_y = Scrollbar(F2, orient=VERTICAL)
-        self.txtarea = Text(F2, font="arial 12", yscrollcommand=scroll_y.set)
+        self.txtarea = Text(F2, font="arial 14", yscrollcommand=scroll_y.set)
         scroll_y.pack(side=RIGHT, fill=Y)
         scroll_y.config(command=self.txtarea.yview)
         self.txtarea.pack(fill=BOTH, expand=1)
-
-        F3 = Frame(self.root)
-        F3.place(x=640, y=30, width=640, height=50)
-        Button(F3,width=12,text="Save as Text",font=("arial", 13), bg="gray80", bd=1, command=self.save).grid(padx=10,pady=3,row=0, column=0)
-        Button(F3,width=15,text="Open in Notepad",font=("arial", 13), bg="gray80", bd=1, command=self.open).grid(padx=10,pady=3,row=0, column=1)
-                
-        F4 = Frame(self.root)
-        F4.place(x=0, y=80, width=640, height=620)
-        #add image in same window
+        
+        F3 = Frame(self.root,bg='cyan3')
+        F3.place(x=280, y=600, width=900, height=50)
+        Button(F3,width=12,text="Close Image",font=("arial", 13), bg="orange", bd=1, command=self.imgclose).grid(padx=15,pady=3,row=0, column=0)
+        Button(F3,width=12,text="Exit",font=("arial", 13), bg="orange", bd=1, command=self.exitwin).grid(padx=15,pady=3,row=0, column=1)
         
     def selectimg(self):
         self.txtarea.delete(1.0,END)
         self.img = askopenfilename(filetypes=[("Image files", ".png .jpg .gif .jpeg")])
-        self.img = cv2.imread(self.img)
-        cv2.imshow('Image',self.img)
+        if self.img!='':
+            self.img = cv2.imread(self.img)
+            self.img = ResizeWithAspectRatio(self.img, width=700)
+            cv2.imshow('Image',self.img)
 
     def scan(self):
-        #self.img = cv2.cvtColor(self.img,cv2.COLOR_BGR2GRAY)
-        self.txtarea.insert(INSERT, pytesseract.image_to_string(self.img))
-        #himg,wimg,a = self.img.shape
-        boxes = pytesseract.image_to_data(self.img)
-        for x,b in enumerate(boxes.splitlines()):
-            if x!=0:
-                b = b.split()
-                if len(b)==12:
-                    x,y,w,h = int(b[6]),int(b[7]),int(b[8]),int(b[9])
-                    cv2.rectangle(self.img,(x,y),(w+x,h+y),(0,0,255),1)
-                    cv2.putText(self.img,b[11],(x,y),cv2.FONT_HERSHEY_COMPLEX,0.5,(50,50.255),1)
-                    cv2.waitKey(30)
-                    cv2.imshow('Image',self.img)
+        if self.img=='':
+            messagebox.showerror("Error","Image not selected")
+        else:
+            cv2.destroyWindow('Image')
+            #self.img = cv2.cvtColor(self.img,cv2.COLOR_BGR2GRAY)
+            self.txtarea.insert(INSERT, pytesseract.image_to_string(self.img))
+            #himg,wimg,a = self.img.shape
+            boxes = pytesseract.image_to_data(self.img)
+            for x,b in enumerate(boxes.splitlines()):
+                if x!=0:
+                    b = b.split()
+                    if len(b)==12:
+                        x,y,w,h = int(b[6]),int(b[7]),int(b[8]),int(b[9])
+                        cv2.rectangle(self.img,(x,y),(w+x,h+y),(0,0,255),1)
+                        cv2.putText(self.img,b[11],(x,y),cv2.FONT_HERSHEY_COMPLEX,0.5,(50,50.255),1)
+                        cv2.waitKey(30)
+                        cv2.imshow('Image',self.img)
 
     def save(self):
-        fname = asksaveasfilename(filetypes=[("Text file", ".txt")])
-        fname = fname if ".txt" in fname else fname + ".txt"
-        f = open(fname, 'w')
-        f.write(self.txtarea.get(1.0,END))
-        f.close()
+        if self.txtarea.get(1.0,END)=='\n':
+            messagebox.showerror("Error","Empty Text")
+        else:
+            fname = asksaveasfilename(filetypes=[("Text file", ".txt")])
+            fname = fname if ".txt" in fname else fname + ".txt"
+            f = open(fname, 'w')
+            f.write(self.txtarea.get(1.0,END))
+            f.close()
 
-    def open(self):
+    def opennp(self):
         fname2 = askopenfilename(filetypes=[("Text file", ".txt")])
-        os.startfile(fname2)
+        if fname2!='':
+            os.startfile(fname2)
+
+    def imgclose(self):
+        cv2.destroyWindow('Image')
+    
+    def exitwin(self):
+        op = messagebox.askyesno("Exit", "Do you really want to exit?")
+        if op > 0:
+            self.root.destroy()
+    
+def ResizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
+        dim = None
+        (h, w) = image.shape[:2]
+
+        if width is None and height is None:
+            return image
+        if width is None:
+            r = height / float(h)
+            dim = (int(w * r), height)
+        else:
+            r = width / float(w)
+            dim = (width, int(h * r))
+
+        return cv2.resize(image, dim, interpolation=inter)    
 
 root = Tk()
 Ocr(root)
